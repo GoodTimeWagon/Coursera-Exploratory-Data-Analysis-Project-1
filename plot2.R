74b42e3d1cc9c5df44e63139f2a05372aef11d31
Old Timer <adjeffre@asu.edu>
2017-04-09 13:18:54 -0700

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,8 @@
+dataFile<-"household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dataSubSet <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(dataSubSet$Global_active_power)
+dataSubSetTime<-as.POSIXct(paste(dataSubSet$Date,dataSubSet$Time),format="%d/%m/%Y %H:%M:%S")
+png("plot2.png", width=480, height=480)
+plot(dataSubSetTime,globalActivePower,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,14 @@
+dataFile<-"household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dataSubSet <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(dataSubSet$Global_active_power)
+dataSubSetTime<-as.POSIXct(paste(dataSubSet$Date,dataSubSet$Time),format="%d/%m/%Y %H:%M:%S")
+subMetering1<-as.numeric(dataSubSet$Sub_metering_1)
+subMetering2<-as.numeric(dataSubSet$Sub_metering_2)
+subMetering3<-as.numeric(dataSubSet$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(dataSubSetTime,subMetering1,type="l",xlab="",ylab="Energy sub metering")
+lines(dataSubSetTime,subMetering2,col="red")
+lines(dataSubSetTime,subMetering3,col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
